4ac55a3c8475bb3848e3f9a94ebb2370f3ab6522
Agata Siemieniec <agata.siemieniec@hotmail.com>
2021-04-05 01:14:19 +0200

    Change import routes at server

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const cors = require("cors");
-const { v4: uuidv4 } = require('uuid');
-const db = require('./db');
 
 const app = express();
 
@@ -10,13 +8,13 @@ const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/', testimonialsRoutes); 
-app.use('/', concertsRoutes); 
-app.use('/', seatsRoutes); 
 
-const message = { message: 'Ok'};
+app.use('/api', testimonialsRoutes); 
+app.use('/api', concertsRoutes); 
+app.use('/api', seatsRoutes); 
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
